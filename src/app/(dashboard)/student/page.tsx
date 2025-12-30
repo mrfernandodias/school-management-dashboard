4301955badee13c2434c0cf992470b1724e6aca5
Fernando Dias <mrfernandodias@gmail.com>
2025-12-29 23:27:57 -0300

    feat: integrate BigCalendar in student, teacher and parent pages

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,5 +1,26 @@
+import Announcements from '@/components/Announcements';
+import BigCalendar from '@/components/BigCalendar';
+import EventCalendar from '@/components/EventCalendar';
+
 const StudentPage = () => {
-  return <div className="">StudentPage</div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-[800px] bg-white p-4 rounded-md flex flex-col">
+          <h1 className="text-xl font-semibold mb-4">Schedule (4A)</h1>
+          <div className="flex-1">
+            <BigCalendar />
+          </div>
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default StudentPage;
--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,5 +1,25 @@
+import Announcements from '@/components/Announcements';
+import BigCalendar from '@/components/BigCalendar';
+import EventCalendar from '@/components/EventCalendar';
+
 const TeacherPage = () => {
-  return <div className="">TeacherPage</div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-[800px] bg-white p-4 rounded-md flex flex-col">
+          <h1 className="text-xl font-semibold mb-4">Schedule</h1>
+          <div className="flex-1">
+            <BigCalendar />
+          </div>
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default TeacherPage;
