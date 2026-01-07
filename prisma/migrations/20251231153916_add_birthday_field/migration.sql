6355503c60c33587f33bfc0b96d2fcb552eb4226
Fernando Dias <mrfernandodias@gmail.com>
2026-01-07 17:15:39 -0300

    chore: add Prisma ORM with schema, migrations and seed data

---
+++ prisma/migrations/20251231153916_add_birthday_field/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `birthday` to the `Parent` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Student` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Teacher` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Parent" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Student" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Teacher" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
