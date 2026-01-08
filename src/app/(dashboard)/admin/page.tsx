f1bd4848f53614bfeba5f2b9a8c30e8fdf31b720
Fernando Dias <mrfernandodias@gmail.com>
2026-01-08 17:46:38 -0300

    refactor: integrate data fetching components in admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,31 +1,31 @@
 import Announcements from '@/components/Announcements';
-import AttendanceChart from '@/components/AttendanceChart';
-import CountChart from '@/components/CountChart';
-import EventCalendar from '@/components/EventCalendar';
+import AttendanceChartContainer from '@/components/AttendanceChartContainer';
+import CountChartContainer from '@/components/CountChartContainer';
+import EventCalendarContainer from '@/components/EventCalendarContainer';
 import FinanceChart from '@/components/FinanceChart';
 import UserCard from '@/components/UserCard';
 
-const AdminPage = () => {
+const AdminPage = ({ searchParams }: { searchParams: { [keys: string]: string | undefined } }) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* UserCard */}
         <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard type="student" />
+          <UserCard type="admin" />
           <UserCard type="teacher" />
+          <UserCard type="student" />
           <UserCard type="parent" />
-          <UserCard type="staff" />
         </div>
         {/* Middle Charts */}
         <div className="flex gap-4 flex-col lg:flex-row">
           {/* CountChart */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart />
+            <CountChartContainer />
           </div>
           {/* AttendanceChart */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart />
+            <AttendanceChartContainer />
           </div>
         </div>
         {/* Bottom Charts */}
@@ -36,7 +36,7 @@ const AdminPage = () => {
 
       {/* Right */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
