3d4d360a86cc1276b567530bcb4d8d999a395777
Fernando Dias <mrfernandodias@gmail.com>
2026-01-08 17:45:47 -0300

    feat: implement database fetch for UserCard and CountChart components

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,15 @@
 import Image from 'next/image';
+import prisma from '@/lib/prisma';
 
-const UserCard = ({ type }: { type: string }) => {
+const UserCard = async ({ type }: { type: 'admin' | 'teacher' | 'student' | 'parent' }) => {
+  const modelMap: Record<typeof type, any> = {
+    admin: prisma.admin,
+    teacher: prisma.teacher,
+    student: prisma.student,
+    parent: prisma.parent,
+  };
+  const data = await modelMap[type].count();
+
   return (
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px] hover:shadow-md transition-shadow cursor-pointer">
       <div className="flex justify-between items-center">
@@ -14,7 +23,7 @@ const UserCard = ({ type }: { type: string }) => {
           className="select-none cursor-pointer hover:opacity-70 transition-opacity"
         />
       </div>
-      <h1 className="text-2xl font-semibold my-4">1.234</h1>
+      <h1 className="text-2xl font-semibold my-4">{data}</h1>
       <h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
     </div>
   );
