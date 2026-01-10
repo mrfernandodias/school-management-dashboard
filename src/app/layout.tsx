40e0813f9cf03f66e3d7e8a0c9587775bf54d439
Fernando Dias <mrfernandodias@gmail.com>
2026-01-10 17:47:22 -0300

    feat(layout): add ToastContainer for notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <ToastContainer position="bottom-right" autoClose={5000} />
+        </body>
       </html>
     </ClerkProvider>
   );
